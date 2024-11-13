6a7bafeb7f6b60f5996b2db3b0dbb465e297cb8a
SamalD007 <samal@smarterdharma.com>
2024-11-13 11:37:08 +0530

    test updates

---
+++ example.test.js
@@ -0,0 +1,10 @@
+// example.test.js
+const add = require('./example');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 1 + 2 to not equal 4', () => {
+  expect(add(1, 2)).not.toBe(4);
+});
